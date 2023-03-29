0f046d320f003ba3671b8ce56c433a1a83a02e87
Aastik Sharma <84064458+aastikvats0109@users.noreply.github.com>
2023-03-29 11:16:55 +0530

    changed the quiz

--- quiz.py
+++ quiz.py
@@ -1,51 +1,419 @@
-import tkinter as tk
- 
-root = tk.Tk()
- 
- 
-def check(answer):
-    if answer[-1] == "*":
-        print("Right, it is {}".format(answer[:-1]))
+print('1.What is the capital of India?')
+print('(A)Chennai        (B)Banglore')
+print('(C)Ranthambore    (D)New  Delhi')
+print('(E)LifeLine(50/50)')
+a=input()
+import sys
+if a=="D":
+      print("Your Answer is correct \nTotal winning: Rs 1000")
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You lose!")
+    sys.exit()
+elif a=="A":
+    print("It is incorrect, You lose!")
+    sys.exit()
+elif a=="E":
+    print('What is the capital of India?')
+    print('               (B)Banglore')
+    print('               (D)New Delhi')
+    b=input()
+    if b=="B":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
     else:
-        print("Wrong")
- 
- 
-class Question:
-    def _init_(self, question):
-        self.question = question
-        self.label = tk.Label(root, text=question)
-        self.label.pack()
- 
- 
-class Answer:
-    def _init_(self, answer):
-        self.answer = answer
-        self.label = tk.Label(root, text=answer[:-1])
-        self.label.bind("<Button-1>", lambda x: check(answer))
-        self.label.pack()
- 
-
- 
- 
-q = """What is the capital of Italy?, Rome*, Paris-, London-,
-What is the capital of France?, Rome-, Paris*, London-,
-Which of these is not a wonder of the world?, Machu Pichu-,Chichen Itza-,Statue of liberty*,
-In which year was MNREGA act introduced?, 2005-,2006*,2007-,
-What is the capital of USA?, New york-,Budapest-,Washington DC*,
-which of the following is known as happiest country?, Finland*,Holland-,Swtzerland-
-"""
- 
-q = q.splitlines()
-q2 = []
-for line in q:
-    q2.append(line.split(","))
-q = q2
- 
-for quest in q:
-    Question(quest[0])
-    for ans in quest[1:]:
-        Answer(and)
- 
- and
-root.mainloop()
-print("End OF Quiz")
+        print("Answer is correct")
+        print("Total winning: Rs 1000")
+print('2.How long did the world war 1 last?')
+print('(A)5years          (B)4years')
+print('(C)6years          (D)7years')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="B":
+      print("Your Answer is correct \nTotal winning: Rs 2000")     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('How long did the world war 1 last?')
+    print('               (B)4years')
+    print('               (D)7years')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 2000")
+print('3.Who was Pablo Escobar?')
+print('(A)Drug Lord       (B)Detective')
+print('(C)Teacher         (D)Driver')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="A":
+      print("Your Answer is correct \nTotal winning: Rs 3000")
+     
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('Who was Pablo Escobar?')
+    print('(A)Drug Lord            ')
+    print('               (D)Driver')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 3000")
+print('4.How many elements are in the periodic table?')
+print('(A)116             (B)117')
+print('(C)118             (D)119')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="C":
+      print("Your Answer is correct \nTotal winning: Rs 4000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('How many elements are in the periodic table?')
+    print('(A)116               ')
+    print('(B)118               ')
+    b=input()
+    if b=="A":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 4000")
+print('5.Which company was initially know as Blue Ribbon Sports?')
+print('(A)Adidas          (B)Nike')
+print('(C)Puma            (D)Reebok')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="B":
+      print("Your Answer is correct \nTotal winning: Rs 5000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('Which company was initially know as Blue Ribbon Sports?')
+    print('               (B)Nike')
+    print('               (D)Reebok')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 5000")
+print('6.In what country is the Chernobyl nuclear plant located?')
+print('(A)Russia          (B)Czech Republic')
+print('(C)Tunisia         (D)Ukraine')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="D":
+      print("Your Answer is correct \nTotal winning: Rs 6000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('?')
+    print('(A)Russia                 ')
+    print('                (D)Ukraine')
+    b=input()
+    if b=="A":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 6000")
+print('7.What is the capital of Canada?')
+print('(A)Ottawa          (B)Toronto')
+print('(C)Vancouver       (D)Calgary')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="A":
+      print("Your Answer is correct \nTotal winning: Rs 7000")
+     
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('What is the capital of Canada?')
+    print('(A)Ottawa                ')
+    print('               (D)Calgary')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 7000")
+print('8.What was the name of the crime boss who was head of the feared Chicago Outfit?')
+print('(A)Pablo Escobar   (B)Carlo Gambino')
+print('(C)Al Capone       (D)Frank Costello')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="C":
+      print("Your Answer is correct \nTotal winning: Rs 8000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('What was the name of the crime boss who was head of the feared Chicago Outfit?')
+    print('                     ')
+    print('(C)Al Capone     (D)Frank Costello')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 8000")
+print('9.How many ghosts chase Pac-Man at the start of each game?')
+print('(A)5               (B)4')
+print('(C)3               (D)6')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="B":
+      print("Your Answer is correct \nTotal winning: Rs 9000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('How many ghosts chase Pac-Man at the start of each game?')
+    print('               (B)4')
+    print('               (D)6')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 9000")
+print('10.What country drinks the most coffee per capita?')
+print('(A)Spain           (B)Canada')
+print('(C)Finland         (D)USA')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="C":
+      print("Your Answer is correct \nTotal winning: Rs 10000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('What country drinks the most coffee per capita?')
+    print('                       ')
+    print('(B)Finland      (D)USA')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 10000")
+print('11.In what country would you find Mount Kilimanjaro?')
+print('(A)Tanzania        (B)Egypt')
+print('(C)Tunisia         (D)Ethiopia')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="A":
+      print("Your Answer is correct \nTotal winning: Rs 11000")
+     
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('In what country would you find Mount Kilimanjaro?')
+    print('(A)Tanzania               ')
+    print('               (D)Ethiopia')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 11000")
+print('12.How many bones do we have in an ear?')
+print('(A)2               (B)3')
+print('(C)5               (D)4')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="B":
+      print("Your Answer is correct \nTotal winning: Rs 12000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('How many bones do we have in an ear?')
+    print('               (B)3')
+    print('               (D)4')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 12000")
+print('13.Which is the only body part that is fully grown from birth?')
+print('(A)Ears            (B)Tongue')
+print('(C)Nose            (D)Eyes')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="D":
+      print("Your Answer is correct \nTotal winning: Rs 13000")
+     
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('Which is the only body part that is fully grown from birth?')
+    print('               (B)Tongue')
+    print('               (D)Eyes')
+    b=input()
+    if b=="B":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 13000")
+print('14.Where is the strongest human muscle located?')
+print('(A)Arms            (B)Legs')
+print('(C)Jaw             (D)Chest')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="C":
+      print("Your Answer is correct \nTotal winning: Rs 14000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="B":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('Where is the strongest human muscle located?')
+    print('                  ')
+    print('(C)Jaw         (D)Chest')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 14000")
+print('15.What height is a regulation NBA basket?')
+print('(A)9feet           (B)10feet')
+print('(C)8feet           (D)12feet')
+print('(E)LifeLine(50/50)')
+a=input()
+if a=="B":
+      print("Your Answer is correct \nTotal winning: Rs 15000")
+     
+elif a=="A":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="C":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="D":
+    print("It is incorrect, You loose!")
+    sys.exit()
+elif a=="E":
+    print('What height is a regulation NBA basket?')
+    print('               (B)10feet')
+    print('               (D)12feet')
+    b=input()
+    if b=="D":
+        print("Answer is incorrect, You loose!")
+        sys.exit()
+    else:
+        print("Answer is correct")
+        print("Total winning: Rs 15000")
